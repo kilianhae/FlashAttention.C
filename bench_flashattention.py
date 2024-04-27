1057e83ce7edfebf7f439aa8397e6660d4100794
Simon Guo <simonguozirui@gmail.com>
2024-04-27 23:30:37 +0000

    add argument for easy profiling and save print attn if correct

--- bench_flashattention.py
+++ bench_flashattention.py
@@ -2,14 +2,23 @@ import math
 import torch
 from torch.nn import functional as F
 from torch.utils.cpp_extension import load
+import argparse
 
 # Load the CUDA kernel as a python module
 minimal_flash = load(name='flash', sources=['src/main.cpp', 'src/flashattention.cu'], extra_cuda_cflags=['-O3'])
 
-batch_size = 2
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch_size', type=int, default=2, help='Batch size')
+parser.add_argument('--seq_len', type=int, default=8192, help='Sequence length')
+args = parser.parse_args()
+
+batch_size = args.batch_size
 n_head = 8
-seq_len = 8192
+seq_len = args.seq_len
 head_embd = 64
+
+print(f"Using {batch_size} batch size, {n_head} heads, {seq_len} sequence length, {head_embd} head embedding size")
+
 torch.cuda.empty_cache()
 
 q = torch.randn(batch_size * n_head, seq_len, head_embd).cuda()
@@ -37,7 +46,10 @@ with torch.autograd.profiler.profile(use_cuda=True) as prof:
 
 print(prof.key_averages().table(sort_by='cuda_time_total', row_limit=10))
 
-print(minimal_flash.cpu())
-print(manual_result.cpu())
-print('attn values sanity check:', torch.allclose(minimal_flash, manual_result, rtol=0, atol=1e-01))
+if torch.allclose(minimal_flash, manual_result, rtol=0, atol=1e-01):
+    print('[Correctness] attn values sanity check: PASSED')
+else:
+    print('Correctness] attn values sanity check: FAILED')
+    print(minimal_flash.cpu())
+    print(manual_result.cpu())
 
