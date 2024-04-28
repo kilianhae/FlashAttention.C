f6b78ca91a61b1d554d5f4e338aed0e6ce513dd9
Simon Guo <simonguozirui@gmail.com>
2024-04-28 15:15:52 +0000

    masked baseline in bench

--- bench_flashattention.py
+++ bench_flashattention.py
@@ -5,19 +5,25 @@ from torch.utils.cpp_extension import load
 import argparse
 
 # Load the CUDA kernel as a python module
+# our flash
 minimal_flash = load(name='flash', sources=['src/main.cpp', 'src/flashattention.cu'], extra_cuda_cflags=['-O3'])
+# integrate llm.c
+#minimal_flash = load(name='flash', sources=['src/main.cpp', 'src/llm.c/attention_forward.cu'], extra_cuda_cflags=['-O3'])
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', type=int, default=2, help='Batch size')
 parser.add_argument('--seq_len', type=int, default=8192, help='Sequence length')
+parser.add_argument('--masking', type=bool, default=True, help='Causal Masking')
 args = parser.parse_args()
 
 batch_size = args.batch_size
 n_head = 8
 seq_len = args.seq_len
 head_embd = 64
+masking = args.masking
 
-print(f"Using {batch_size} batch size, {n_head} heads, {seq_len} sequence length, {head_embd} head embedding size")
+print(f"Using {batch_size} batch size, {n_head} heads, {seq_len} sequence length, {head_embd} head embedding size, {'with' if masking else 'without'} causal masking")
 
 torch.cuda.empty_cache()
 
@@ -27,11 +33,21 @@ v = torch.randn(batch_size * n_head, seq_len, head_embd).cuda()
        
 
 # Compare to Pytroch's matmul
-def manual_attention(q, k):
+def manual_attention_unmasked(q, k):
     S = torch.matmul(q, k.transpose(-2, -1))#/math.sqrt(head_embd)
     A = F.softmax(S, dim=-1)
     O = torch.matmul(A, v)
     return O
+
+def manual_attention_masking(q, k):
+    S = torch.matmul(q, k.transpose(-2, -1))#/math.sqrt(head_embd)
+    mask = torch.triu(torch.ones(seq_len, seq_len), diagonal=1).bool().cuda()
+    S.masked_fill_(mask, -float('inf'))
+    A = F.softmax(S, dim=-1)
+    O = torch.matmul(A, v)
+    return O
+
+manual_attention = manual_attention_masking if masking else manual_attention_unmasked
 
 print('=== profiling manual attention ===')
 with torch.autograd.profiler.profile(use_cuda=True) as prof:
